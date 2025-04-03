aa40215d9ed8cc3f17d9846400a12b54367e1e51
Windy <lt33562@gmail.com>
2025-04-03 22:25:52 +0700

    Initial commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+export default function LoginPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-center text-2xl font-bold">Lê Xuân Thông Xin chào NextJS</h1>
+    </div>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function LoginPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-center text-2xl font-bold">Lê Xuân Thông Xin chào NextJS</h1>
+    </div>
+  );
+}
